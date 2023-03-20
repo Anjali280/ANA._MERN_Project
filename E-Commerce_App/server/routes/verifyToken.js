80b24fe8eab85715e51fd2b7e36ce93bda6e1b67
Anjali280 <mondalanjali0707@gmail.com>
2023-03-21 00:36:08 +0530

    PUT functionality added

---
+++ E-Commerce_App/server/routes/verifyToken.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+
+/* This is the middleware , that is after veryfying a user through token put method can be done */
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    console.log(token);
+    jwt.verify(token, process.env.JWT_SECRECT_KEY, (err, user) => {
+      if (err) {
+        res.status(403).json("Token is not valid!");
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).json("You are not authenticated!");
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+module.exports = { verifyToken, verifyTokenAndAuthorization };
