e9d0cf58dbaac788fca2d5f9a9140d6aabe374b1
Anjali280 <mondalanjali0707@gmail.com>
2023-03-23 23:58:52 +0530

    ..

--- E-Commerce_App/CLIENT/src/App.jsx
+++ E-Commerce_App/CLIENT/src/App.jsx
@@ -4,16 +4,25 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+            <Cart />
+          </Route>
+        </Switch>
+      </Router>
+
       {/* <ProductList /> */}
       {/* <Product /> */}
       {/* <Register /> */}
       {/* <Login /> */}
-      {/* <Cart /> */}
+      
     </div>
   );
 }
